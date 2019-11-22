508838a7ef6c213a2390f77313c39f7f0a895b2c
coder-aman-droid <aman.offficial@gmail.com>
2019-11-23 00:34:21 +0530

    ques10-v2

--- MaxSubArrayCount.java
+++ MaxSubArrayCount.java
@@ -3,24 +3,18 @@ import java.util.Scanner;
 public class MaxSubArrayCount{
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int count = 0;
 		int n = sc.nextInt();
 		int[] arr = new int[n];
 		for(int i=0;i<n;i++)
 			arr[i] = sc.nextInt();
 		int x = sc.nextInt();
-		for(int i=0;i<n;i++) {
-			for(int j=i;j<n;j++) {
-				int max = Integer.MIN_VALUE;
-				for(int k=i;k<=j;k++) {
-					if(arr[k]>max) {
-						max = arr[k];
-					}
-				}
-				if(max > x)
-					count++;
-			}
-		}
-		System.out.println(count);
+		int totalSubArrays = n*(n+1)/2;
+		int m = 0;
+		for(int i=0;i<n;i++)
+			if(arr[i]<=x)
+				m++;
+		int dontCount = m*(m+1)/2;
+		int countedSubArrays = totalSubArrays - dontCount;
+		System.out.println(countedSubArrays);
 	}
 }
