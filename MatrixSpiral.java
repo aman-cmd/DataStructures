4eb1d3b374528fb8dcedcb7ee2261dcac97c3878
coder-aman-droid <aman.offficial@gmail.com>
2019-12-12 18:46:12 +0530

    ques28

--- MatrixSpiral.java
+++ MatrixSpiral.java
@@ -9,13 +9,27 @@ public class MatrixSpiral{
 		for(int i=0;i<n;i++)
 			for(int j=0;j<m;j++)
 				arr[i][j] = sc.nextInt();
-		for(int i=0;i<n;i++) {
-			if(i%2==0)
-				for(int j=0;j<m;j++)
-					System.out.print(arr[i][j]+" ");
-			else
-				for(int j=m-1;j>=0;j--)
-					System.out.print(arr[i][j]+" ");
+		int r_start = 0;
+		int r_end = n-1;
+		int c_start = 0;
+		int c_end = m-1;
+		while(r_start<r_end&&c_start<c_end) {
+			for(int i=c_start;i<=c_end;i++) {
+				System.out.print(arr[r_start][i]+" ");
+			}
+			r_start++;
+			for(int i=r_start;i<=r_end;i++) {
+				System.out.print(arr[i][c_end]+" ");
+			}
+			c_end--;
+			for(int i=c_end;i>=c_start;i--) {
+				System.out.print(arr[r_end][i]+" ");
+			}
+			r_end--;
+			for(int i=r_end;i>=r_start;i--) {
+				System.out.print(arr[i][c_start]+" ");
+			}
+			c_start++;
 		}
 	}
 }
