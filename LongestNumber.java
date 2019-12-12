1da66c1255c4899a4c9f5dc976bee27034abbf10
coder-aman-droid <aman.offficial@gmail.com>
2019-12-12 19:18:04 +0530

    ques29

---
+++ LongestNumber.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LongestNumber{
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++)
+			arr[i] = sc.nextInt();
+		String[] strarr = new String[n];
+		for(int i=0;i<n;i++) {
+			strarr[i] = String.valueOf(arr[i]);
+		}
+		Arrays.sort(strarr);
+		String res = "";
+		for(int i=n-1;i>=0;i--) {
+			res += strarr[i];
+		}
+		
+		System.out.println(Long.parseLong(res));
+	}
+}
